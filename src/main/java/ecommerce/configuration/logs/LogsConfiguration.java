f0d47a1480ef5cc63670ebac1f2b8f65e9903607
OverworkedCriminal <tom.swirgon@gmail.com>
2024-09-26 14:18:45 +0200

    initial logs configuration step 1

---
+++ src/main/java/ecommerce/configuration/logs/LogsConfiguration.java
@@ -0,0 +1,18 @@
+package ecommerce.configuration.logs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class LogsConfiguration
+{
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        final var filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeClientInfo(true);
+        filter.setIncludeQueryString(true);
+        return filter;
+    }
+}
