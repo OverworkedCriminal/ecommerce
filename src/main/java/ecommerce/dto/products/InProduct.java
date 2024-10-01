4801b4bf8eeb51b3db0a5a2fab58cfd37c98593b
OverworkedCriminal <tom.swirgon@gmail.com>
2024-10-01 15:49:49 +0200

    replace lombok's NonNull with jakarta.validation NotNull

--- src/main/java/ecommerce/dto/products/InProduct.java
+++ src/main/java/ecommerce/dto/products/InProduct.java
@@ -4,10 +4,10 @@ import java.math.BigDecimal;
 
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
-import lombok.NonNull;
+import jakarta.validation.constraints.NotNull;
 
 public record InProduct(
     @NotBlank String name,
     @NotBlank String description,
-    @NonNull @DecimalMin(value = "0.00", inclusive = false) BigDecimal price
+    @NotNull @DecimalMin(value = "0.00", inclusive = false) BigDecimal price
 ) {}
